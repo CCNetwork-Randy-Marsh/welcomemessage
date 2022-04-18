37963642b7e39cc4aedd025d2c9a81b16fe04cb2
CCNetwork-Randy-Marsh <lodberglars@gmail.com>
2022-04-18 04:27:56 +0200

    Update and rename readme.md to test.php

--- wiki/readme.md
+++
@@ -1,2 +0,0 @@
-![](https://i.imgur.com/5wk9NGi.png)
-![](https://cf.way2muchnoise.eu/full_609702_downloads.svg)
---
+++ wiki/test.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="copyright" content="Iskallia" />
+    <meta name="language" content="en" />
+    <meta name="robots" content="index, follow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Facebook -->
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:site_name" content="Vault Hunters" />
+    <meta property="og:title" content="Vault Hunters" />
+    <meta property="og:url" content="https://www.vaulthunters.gg" />
+    <meta property="og:type" content="website" />
+    <meta
+      property="og:description"
+      content="Build an amazing base, level up and spend skill points to grow stronger, conquer the vaults and complete the artifact puzzle! - Do you have what it takes?"
+    />
+    <meta
+      property="og:image"
+      content="https://www.vaulthunters.gg/og-img.png"
+    />
+
+    <!-- Twitter -->
+    <meta name="twitter:title" content="Vault Hunters" />
+    <meta name="twitter:url" content="https://www.vaulthunters.gg" />
+    <meta
+      name="twitter:description"
+      content="Build an amazing base, level up and spend skill points to grow stronger, conquer the vaults and complete the artifact puzzle! - Do you have what it takes?"
+    />
+    <meta
+      name="twitter:image"
+      content="https://www.vaulthunters.gg/og-img.png"
+    />
+    <meta name="twitter:card" content="summary" />
+
+    <!-- SEO -->
+    <meta rel="canonical" href="https://www.vaulthunters.gg" />
+    <meta rel="shortlink" href="https://www.vaulthunters.gg" />
+    <meta
+      name="description"
+      content="Build an amazing base, level up and spend skill points to grow stronger, conquer the vaults and complete the artifact puzzle! - Do you have what it takes?"
+    />
+    <meta name="author" content="Iskallia" />
+    <meta name="publisher" content="Iskallia" />
+
+    <!-- icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+    <meta name="msapplication-TileColor" content="#000000" />
+    <meta name="theme-color" content="#ffffff" />
+
+    <title>Vault Hunters - Minecraft Modpack</title>
+    <script type="module" crossorigin src="/assets/index.acadc341.js"></script>
+    <link rel="modulepreload" href="/assets/vendor.c6e3325f.js">
+    <link rel="stylesheet" href="/assets/index.bd38a52d.css">
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+    
+  </body>
+</html>
